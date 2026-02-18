b3c60e32802136be46c3fc730cb59707224f9095
Daniela Noya Velazco <168275198+Dnv2005@users.noreply.github.com>
2026-02-18 13:26:46 +0100

    Esto no pinta nada aqui

--- Downloads/taskapp/taskapp-v1/ui/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Inyectamos la variable de entorno API_URL en un archivo JS "config.js" accesible desde la UI
-echo "window.__API_URL__ = '${API_URL}';" > /usr/share/nginx/html/config.js
-
-# Arrancamos nginx en primer plano
-exec "$@"
